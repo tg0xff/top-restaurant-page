d605e897609ed89d41123ef9a4f1d5f2ef7603e5
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-07 16:45:12 -0600

    Replace import statements with require() calls

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import path from "path";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
