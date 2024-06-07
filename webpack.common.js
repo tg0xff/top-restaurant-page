340383a82ae5df8643f9d931ce1869aa2a880717
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-07 17:57:35 -0600

    Configure webpack asset management for images

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,12 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
