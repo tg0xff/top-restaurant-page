80dac74334283b8d2f235d917726c863111f5f63
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-07 16:38:08 -0600

    Configure webpack to use HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "path";
 
 module.exports = {
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "head",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
