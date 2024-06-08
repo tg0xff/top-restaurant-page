88b7945ddbf4cafd1d5a2c75e5ff6ff0fe865da9
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-07 19:07:21 -0600

    Move h1 element to JS

--- src/index.js
+++ src/index.js
@@ -11,3 +11,7 @@ figcaption.innerHTML =
   'Photo by <a href="https://unsplash.com/@nik_owens?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nik Owens</a> on <a href="https://unsplash.com/photos/pizza-on-brown-wooden-table-40OJLYVWeeM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
 figure.appendChild(figcaption);
 content.appendChild(figure);
+
+const h1 = document.createElement("h1");
+h1.textContent = "Mario's Italian Restaurant";
+content.appendChild(h1);
