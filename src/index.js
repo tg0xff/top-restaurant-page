6602592b246017ec0bf7be0ad600579e9d0de34a
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-07 19:05:42 -0600

    Move figure element to JS

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Photo from "./nik-owens-40OJLYVWeeM-unsplash.jpg";
+
+const content = document.querySelector("#content");
+
+const figure = document.createElement("figure");
+const img = new Image();
+img.src = Photo;
+figure.appendChild(img);
+const figcaption = document.createElement("figcaption");
+figcaption.innerHTML =
+  'Photo by <a href="https://unsplash.com/@nik_owens?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nik Owens</a> on <a href="https://unsplash.com/photos/pizza-on-brown-wooden-table-40OJLYVWeeM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
+figure.appendChild(figcaption);
+content.appendChild(figure);
