3ed017d43591b181c996ef7eb12c77687963b485
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-07 19:31:22 -0600

    Add css-loader module to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,10 @@ module.exports = {
         loader: "html-loader",
       },
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
