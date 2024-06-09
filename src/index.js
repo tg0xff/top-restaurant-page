c37ba4c475a3e6c89fbdaa917c8e587a6515547b
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-08 20:06:39 -0600

    Define renderPage()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import "./style.css";
-import home from "./home.js"
+import home from "./home.js";
+
+const body = document.querySelector("body");
+
+function renderPage(e) {
+  let content = body.querySelector("#content");
+  content.parentElement.removeChild(content);
+
+  content = document.createElement("div");
+  content.setAttribute("id", "content");
+  content.classList.add("content");
+  body.appendChild(content);
+
+  switch (e.target.getAttribute("id")) {
+    case "home":
+      home();
+      break;
+  }
+}
 
 home();
