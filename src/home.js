57ae53344720014fca25cac2beee360704bce4f9
Selene Tamez Galván <tg.sln255@gmail.com>
2024-06-09 19:41:22 -0600

    Rename the restaurant to 'Pizza Restaurant'

--- src/home.js
+++ src/home.js
@@ -14,7 +14,7 @@ export default function () {
     'Photo by <a href="https://unsplash.com/@nik_owens?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nik Owens</a> on <a href="https://unsplash.com/photos/pizza-on-brown-wooden-table-40OJLYVWeeM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>';
   figure.appendChild(figcaption);
   const h1 = document.createElement("h1");
-  h1.textContent = "Mario's Italian Restaurant";
+  h1.textContent = "Mario's Pizza Restaurant";
   hero.appendChild(h1);
   content.appendChild(hero);
 }
